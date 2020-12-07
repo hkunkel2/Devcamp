f12b2125e2a43e55935ea7efe2e2ce1f8969c8cb
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 00:03:03 -0500

    added portfolio items via Resource generator

---
+++ db/migrate/20201207045644_create_portolios.rb
@@ -0,0 +1,13 @@
+class CreatePortolios < ActiveRecord::Migration[6.0]
+  def change
+    create_table :portolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
