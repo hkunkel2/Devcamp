3032be55d8930fa6afcc04ad0f3ebaf1c2a669a6
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 03:07:26 -0500

    Integrated destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,9 +42,18 @@ class PortfoliosController < ApplicationController
     end
 
     def destroy
+        # Perform the lookup
+        @portfolio_item = Portfolio.find(params[:id])
     
-    end
-
+        # Destroy/delete the record
+        @portfolio_item.destroy
+    
+        # Redirect
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+        end
+      end
+    
     private
     def portfolio_params
         params.require(:portfolio).permit(:title, :subtitle, :body)
