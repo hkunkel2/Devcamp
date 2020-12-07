21e7f3c912de89a03ce6166d8806b81ca574c1fb
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 03:24:12 -0500

    Customized show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :portfolios
+  resources :portfolios, expect: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
