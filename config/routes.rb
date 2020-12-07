5ab56819f1dc3d9c5f6a81d366a0616df812145d
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 12:54:06 -0500

    Integrated draft and published enum to blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
   root to: 'pages#home'
+
   resources :portfolios, expect: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
