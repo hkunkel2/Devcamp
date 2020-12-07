ff4f01879d511ed242e650b39c3c286b64a38282
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 03:01:03 -0500

    Integrated show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,22 +25,22 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
       end
     
-      def update
+    def update
         @portfolio_item = Portfolio.find(params[:id])
     
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
-          else
-            format.html { render :edit }
-          end
+            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+                format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+            else
+                format.html { render :edit }
+            end
         end
-      end
-    
-    def show
-    
     end
-    
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
     def destroy
     
     end
