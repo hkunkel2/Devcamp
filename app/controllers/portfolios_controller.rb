5048d459bd54fdea589458f42dea24b43ca382f1
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-07 02:44:49 -0500

    Integrated create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,11 +5,20 @@ class PortfoliosController < ApplicationController
     end
     
     def new
-    
+        @portfolio_item = Portfolio.new
     end
     
     def create
-    
+        @portfolio_item = Portfolio.new(portfolio_params)
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+                format.json { render :show, status: :created, location: @portfolio_item }
+            else
+                format.html { render :new }
+                format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+            end
+        end
     end
     
     def edit
@@ -26,6 +35,11 @@ class PortfoliosController < ApplicationController
     
     def destroy
     
+    end
+
+    private
+    def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
     end
 
 end
