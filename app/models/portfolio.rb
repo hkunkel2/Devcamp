ffc9c9cd053150c9ba3e6d057049ee160a66a019
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-12-12 23:25:17 -0500

    Implemneted pictures for profiles defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,5 +6,12 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
-    
+
+
+    after_initialize :set_default
+
+    def set_default
+        self.main_image ||= "https://cdn.pixabay.com/photo/2020/11/22/20/39/giraffe-5767909_1280.jpg"
+        self.thumb_image ||= "https://cdn.pixabay.com/photo/2019/01/26/10/24/eagle-3955951_1280.jpg"
+    end
 end
