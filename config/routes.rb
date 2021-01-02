bfcd6b3a098089078fa33a187a6dc6d170289135
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2021-01-02 17:23:22 -0500

    Implemented tech-new pasge with tweets

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
